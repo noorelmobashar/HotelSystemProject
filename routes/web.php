7dab259a5f7814feb98a4b047f8a824193632ee3
abdelftah shkal <abdelftahshkal@gmail.com>
2026-03-29 20:15:10 +0200

    resolve the merge

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+<<<<<<< HEAD
+use Illuminate\Http\Request;
+=======
 use App\Http\Controllers\ReceptionistController;
 use App\Http\Controllers\ReservationController;
-use Illuminate\Http\Request;
+use Illuminate\Foundation\Application;
+>>>>>>> 611f1edc4f2ed0973715d150dc1bc2e3409a6850
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -26,6 +30,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+<<<<<<< HEAD
+=======
 
     Route::get('/reservations', [ReservationController::class, 'index'])->name('reservations.index');
     Route::get('/reservations/create', [ReservationController::class, 'create'])->name('reservations.create');
@@ -37,6 +43,29 @@ Route::middleware('auth')->group(function () {
         Route::put('/receptionists/{receptionist}', [ReceptionistController::class, 'update'])->name('receptionists.update');
         Route::delete('/receptionists/{receptionist}', [ReceptionistController::class, 'destroy'])->name('receptionists.destroy');
     });
+>>>>>>> 611f1edc4f2ed0973715d150dc1bc2e3409a6850
 });
 
-require __DIR__ . '/auth.php';
+//add the route in the gruop  depend on the role
+//admin
+//manager
+//receptionist
+//client
+
+
+// Route::middleware(['auth', 'role:admin'])->group(function () {
+// });
+
+
+// Route::middleware(['auth', 'role:manager'])->group(function () {
+// });
+
+// Route::middleware(['auth', 'role:manager|admin'])->group(function () {
+// });
+
+// Route::middleware(['auth', 'role:receptionist'])->group(function () {
+// });
+
+
+
+require __DIR__.'/auth.php';
